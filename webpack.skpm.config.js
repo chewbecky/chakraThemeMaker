01d1b9e0c9de0e41c100b42dbec0bea9f808781f
Tim Kolberger <tim@kolberger.eu>
2021-02-14 12:06:44 +0100

    :building_construction: Alter webpack config to transpile all .ts and .tsx files

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -6,54 +6,57 @@ module.exports = function (config, entry) {
     : {
         setImmediate: false,
       };
-  config.module.rules.push({
-    test: /\.(html)$/,
-    use: [
-      {
-        loader: "@skpm/extract-loader",
-      },
-      {
-        loader: "html-loader",
-        options: {
-          attrs: ["img:src", "link:href"],
-          interpolate: true,
+  config.resolve.extensions.push(".ts", ".tsx");
+  config.module.rules.push(
+    {
+      test: /\.(html)$/,
+      use: [
+        {
+          loader: "@skpm/extract-loader",
         },
-      },
-    ],
-  });
-  config.module.rules.push({
-    test: /\.(css)$/,
-    use: [
-      {
-        loader: "@skpm/extract-loader",
-      },
-      {
-        loader: "css-loader",
-      },
-    ],
-  });
-  config.module.rules.push({
-    test: /\.(js|jsx)$/,
-    exclude: /node_modules/,
-    use: [
-      {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+        {
+          loader: "html-loader",
+          options: {
+            attrs: ["img:src", "link:href"],
+            interpolate: true,
+          },
         },
-      },
-    ],
-  });
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    exclude: [/node_modules/],
-    use: [
-      {
-        loader: "ts-loader",
-        options: {
-          configFile: path.resolve(process.cwd(), "tsconfig.json"),
+      ],
+    },
+    {
+      test: /\.(css)$/,
+      use: [
+        {
+          loader: "@skpm/extract-loader",
         },
-      },
-    ],
-  });
+        {
+          loader: "css-loader",
+        },
+      ],
+    },
+    {
+      test: /\.(ts|tsx)$/,
+      exclude: [/node_modules/],
+      use: [
+        {
+          loader: "ts-loader",
+          options: {
+            configFile: path.resolve(process.cwd(), "tsconfig.json"),
+          },
+        },
+      ],
+    },
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      ],
+    }
+  );
 };
