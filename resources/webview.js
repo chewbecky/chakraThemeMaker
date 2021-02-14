46cf823b2703aa7f5119642233e15135d5d7251d
Tim Kolberger <tim@kolberger.eu>
2021-02-14 12:11:21 +0100

    :sparkles: Use Chakra UI and the new communication hooks in the webview

--- resources/webview.js
+++ resources/webview.js
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.tsx";
-
-// call the plugin from the webview
-document.getElementById("button").addEventListener("click", () => {
-  window.postMessage("nativeLog", "Called from the webview");
-});
-
-window.sendData = (data) => {
-  // create and dispatch the event including the data
-  let event = new CustomEvent("send-data", {
-    detail: { data },
-  });
-  window.dispatchEvent(event);
-};
+import App from "./components/App";
 
 (() => {
   ReactDOM.render(<App />, document.getElementById("root"));
